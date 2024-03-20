5f7a9db567c6a152be83a4fb95500666715e5754
Sladjan <sladjanstojcic@gmail.com>
2024-03-20 14:44:48 +0100

    feat(app): remove unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={() => toast('Hello toast!')}>Show Toast</button>
+            <ToastContainer />
+        </div>
+    );
 }
 
 export default App;
